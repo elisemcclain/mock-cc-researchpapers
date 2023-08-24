e4ed75929d859d2c379c3b1776ea86aa438125ad
elisemcclain <celisemcclain@gmail.com>
2023-08-23 20:27:45 -0500

    research papers mcc4

--- server/app.py
+++ server/app.py
@@ -14,14 +14,70 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+api = Api(app)
+
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/research')
-def restaurants():
+# is it ok that my research class is the same in models AND app?
+class ResearchRes(Resource):
+    def get(self):
+        research = [r.to_dict(rules=('-research_authors', '-created_at', '-updated_at',)) for r in Research.query.all()]
+        return make_response(research, 200)
 
-    pass
+api.add_resource(ResearchRes, '/research')
+
+class ResearchById(Resource):
+    def get(self, id):
+        research = Research.query.filter(Research.id == id).first()
+
+        if not research:
+            return make_response({"error": "Research paper not found"}, 404)
+        
+        return make_response(research.to_dict(rules=('-created_at', '-updated_at',)), 200)
+
+    def delete(self, id):
+        research = Research.query.filter(Research.id == id).first()
+
+        if not research:
+            return make_response({"error": "Research paper not found"}, 404)
+
+        if research:
+            db.session.delete(research)
+            db.session.commit()
+        return make_response({}, 204)
+
+
+api.add_resource(ResearchById, '/research/<int:id>')
+
+class Authors(Resource):
+    def get(self):
+        author = [a.to_dict(rules=('-research_authors', '-created_at', '-updated_at',)) for a in Author.query.all()]
+        return make_response(author, 200)
+
+api.add_resource(Authors, '/authors')
+
+class ResearchAuthorsR(Resource):
+    def post(self):
+        new_research_author = ResearchAuthors()
+        data = request.get_json()
+
+        try:
+            for key in data:
+                setattr(new_research_author, key, data[key])
+            
+            db.session.add(new_research_author)
+            db.session.commit()
+            return make_response(new_research_author.author.to_dict(), 201)
+        
+        except ValueError as error:
+            new_error = {"errors": str(error)}
+            return make_response(new_error, 400)
+
+api.add_resource(ResearchAuthorsR, '/research_authors')
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,68 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Research(db.Model, SerializerMixin):
+    __tablename__ = 'researchs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String)
+    year = db.Column(db.Integer)
+    page_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    research_authors = db.relationship('ResearchAuthors', cascade='all, delete', backref='research')
+
+    serialize_rules = ('-research_authors.research', '-created_at', '-updated_at',)
+
+    # TESTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:
+    authors = association_proxy('research_authors', 'author')
+
+    @validates('year')
+    def validate_year(self, key, year):
+        if not 1000 <= year <= 9999:
+            raise ValueError('please give a valid year')
+        return year
+        
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    research_authors = db.relationship('ResearchAuthors', cascade='all, delete', backref='author')
+
+    serialize_rules = ('-research_authors.author', '-created_at', '-updated_at',)
+
+    # TESTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:
+    serialize_only = ('id', 'name', 'field_of_study',)
+
+
+    @validates('field_of_study')
+    def validate_field_of_study(self, key, field_of_study):
+        allowed_fields = ['AI', 'Robotics', 'Machine Learning', 'Vision', 'Cybersecurity']
+        if field_of_study not in allowed_fields:
+            raise ValueError('please give a valid study')
+        return field_of_study
+
+
+
+class ResearchAuthors(db.Model, SerializerMixin):
+    __tablename__ = 'research_authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    research_id = db.Column(db.Integer, db.ForeignKey('researchs.id'))
+
+    serialize_rules = ('-author.research_authors', '-research.research_authors', '-created_at', '-updated_at',)
+
+    # TESTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:
+    # serialize_only = ('id', 'name', 'field_of_study',)
